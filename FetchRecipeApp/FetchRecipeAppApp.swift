3e1b1e79de1b1fb86231ed136b3f9874d2b3ddb8
Luis Arias <luis.arias@encora.com>
2024-10-04 18:19:33 -0600

    Initial Commit

---
+++ FetchRecipeApp/FetchRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeAppApp.swift
+//  FetchRecipeApp
+//
+//  Created by Luis Arias on 04/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
